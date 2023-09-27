c1c88b5b1991d2d10067a64ceadd6b0753d09cbc
dkcr7 <dikshant.bali@gmail.com>
2023-09-27 18:16:16 +0530

    Your Message

---
+++ src/main/java/com/dikshant/utility/VersionUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.dikshant.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionUpdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dikshant/utility/VersionUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikshant.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
